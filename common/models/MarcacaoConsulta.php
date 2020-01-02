257e777e4c9ecc6f88116ebe2c32b8963d9f8c31
JoaoAlvega <50098045+JoaoAlvega@users.noreply.github.com>
2020-01-02 23:03:32 +0000

    Mosquitto (versao1)

--- common/models/MarcacaoConsulta.php
+++ common/models/MarcacaoConsulta.php
@@ -2,6 +2,7 @@
 
 namespace common\models;
 
+use app\mosquitto\phpMQTT;
 use Yii;
 
 /**
@@ -71,5 +72,59 @@ class MarcacaoConsulta extends \yii\db\ActiveRecord
     public function getPessoa()
     {
         return $this->hasOne(Pessoa::className(), ['idPessoa' => 'Pessoa_idPessoa']);
+    }
+
+    public function afterSave($insert, $changedAttributes)
+    {
+        parent::afterSave($insert, $changedAttributes);
+
+        $idMarcacao_Consulta = $this->idMarcacao_Consulta;
+        $Pessoa_idPessoa = $this->Pessoa_idPessoa;
+        $Consulta_idConsulta = $this->Consulta_idConsulta;
+        $Estado = $this->Estado;
+        $Descricao = $this->Descricao;
+        $Urgente = $this->Urgente;
+
+
+        $myObj = new \stdClass();
+        $myObj->id = $idMarcacao_Consulta;
+        $myObj->id = $Pessoa_idPessoa;
+        $myObj->id = $Consulta_idConsulta;
+        $myObj->id = $Estado;
+        $myObj->id = $Descricao;
+        $myObj->id = $Urgente;
+
+        $myJSON = json_encode($myObj);
+        if ($insert)
+            $this->FazPublish("INSERT", $myJSON);
+        else
+            $this->FazPublish("UPDATE", $myJSON);
+    }
+
+    public function afterDelete()
+    {
+        parent::afterDelete();
+        $prod_id = $this->idMarcacao_Consulta;
+        $myObj = new \stdClass();
+        $myObj->id = $prod_id;
+        $myJSON = json_encode($myObj);
+        $this->FazPublish("DELETE", $myJSON);
+    }
+
+    public function FazPublish($canal, $msg)
+    {
+        $server = "127.0.0.1";
+        $port = 1883;
+        $username = ""; // set your username
+        $password = ""; // set your password
+        $client_id = "phpMQTT-publisher"; // unique!
+        $mqtt = new phpMQTT($server, $port, $client_id);
+        if ($mqtt->connect(true, NULL, $username, $password)) {
+            $mqtt->publish($canal, $msg, 0);
+            $mqtt->close();
+
+
+        }
+        else {file_put_contents("debug.output","Time out");}
     }
 }
