7b1743886bb57b16dc616a7a0ca4f5eff0ea59bd
JoaoAlvega <50098045+JoaoAlvega@users.noreply.github.com>
2020-01-14 00:34:05 +0000

    Revert "teste ftecnica (completa)"
    This reverts commit b9aacfab2bbb65313d25702c3a4807440d101974.

--- backend/tests/unit/FtecnicaTest.php
+++ backend/tests/unit/FtecnicaTest.php
@@ -32,7 +32,7 @@ class FtecnicaTest extends \Codeception\Test\Unit
         $fichatecnica->Observacoes = "teste";
         $fichatecnica->Consulta_idConsulta= "1";
 
-        return $fichatecnica;
+        $fichatecnica->save();
     }
 
     public function testFtecnicaValida()
@@ -44,33 +44,11 @@ class FtecnicaTest extends \Codeception\Test\Unit
 
     public function testConsulta_idConsultaFtecnicaVazio()
     {
-        $fichatecnica = $this->getFtecnicaValida();
+        $fichatecnica = new FichaTecnica();
 
         $fichatecnica->Consulta_idConsulta = "";
 
         $this->assertTrue($fichatecnica->validate());
     }
-
-    public function testAtualizarRegisto()
-    {
-
-        $this->tester->haveRecord(FichaTecnica::class, ['Ficheiro' => 'teste alterado', 'Observacoes' => 'teste alterado', 'Consulta_idConsulta' => '1']);
-
-        $u = FichaTecnica::findOne(['Ficheiro' => 'teste alterado']);
-        $u->Ficheiro = "asd";
-        $u->save();
-
-    }
-
-    public function testRemoverRegisto()
-    {
-        $this->tester->haveRecord(FichaTecnica::class, ['Ficheiro' => 'teste alterado', 'Observacoes' => 'teste alterado', 'Consulta_idConsulta' => '1'], "Registo inexistente!");
-
-        $u = FichaTecnica::findOne(['Ficheiro' => 'teste alterado']);
-        $u->delete();
-
-        $this->tester->cantSeeRecord(FichaTecnica::class, ['Ficheiro' => 'teste alterado', 'Observacoes' => 'teste alterado', 'Consulta_idConsulta' => '1']);
-    }
-
 
 }
